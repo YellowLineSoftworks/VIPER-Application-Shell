8e83ecfb648716ee26bac0b359c9bb8977b345b3
Jack Hamilton <jackham800@gmail.com>
2013-11-03 01:10:33 -0400

    Viper 1.1 Alpha
    Added Button class

---
+++ src/Button.java
@@ -0,0 +1,17 @@
+
+import resources.Sprite;
+
+/**
+ * @author Jack
+ */
+public class Button extends resources.gameobjects.Button{
+
+    public Button(int x, int y, Sprite unclicked, Sprite clicked, int buttonType) {
+        super(x, y, unclicked, clicked, buttonType);
+    }
+    
+    public void clicked(int buttonType) {
+        
+    }
+
+}
